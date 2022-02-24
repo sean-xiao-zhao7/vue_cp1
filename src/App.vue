e25522593b73521e9a22a1a14fb7741eba39f75d
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-02-24 14:34:25 -0500

    Set up

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      resources: [
+        {
+          id: 'tesatetat',
+          title: 'Res 1',
+        },
+      ],
+    };
+  },
+};
+</script>
